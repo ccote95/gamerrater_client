65481ed590f66c41c3c9bcd0961782c0742304d4
Chad Cote <chadcote58@gmail.com>
2024-08-03 11:34:38 -0400

    Created a fetch to send the new game to the api

--- src/Managers/gameManager.js
+++ src/Managers/gameManager.js
@@ -18,3 +18,15 @@ export const FetchSingleGame = async (id) => {
     const game = await response.json()
     return game
 }
+
+export const SubmitGame = async (game) => {
+    await fetch("http://localhost:8000/games",
+        {
+            method: "POST",
+            headers: {
+                "Authorization": `Token${JSON.parse(localStorage.getItem("rock_token")).token}`
+            },
+            body: JSON.stringify(game)
+        }
+    )
+}
