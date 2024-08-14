632c094bffe9727a4494c17119d27fb78df086f2
Chad Cote <chadcote58@gmail.com>
2024-08-14 10:55:23 -0400

    Created the seach bar and added logic to the fetch to handle the search

--- src/Components/Pages/allGames.jsx
+++ src/Components/Pages/allGames.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { FetchAllGames } from "../../Managers/gameManager.js"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { Badge, Button, Card, CardTitle, Input, InputGroup } from "reactstrap"
 
 
 export const AllGames = () => {
@@ -16,12 +17,25 @@ export const AllGames = () => {
 
     return (
         <div>
-            <button onClick={() => {navigate("/create")}}>Register A New Game</button>
+            <Button className="btn-success" onClick={() => {navigate("/create")}}>Register A New Game</Button>
+            <InputGroup className="w-25" style={{float: "right"}}>
+                <Input type="text"/>
+                <Button>Search</Button>
+
+            </InputGroup>
         <div>
             {games.map(game => 
-                <div key={game.id}>
-                    <a href={`allgames/${game.id}`}>{game.title}</a>
-                </div>
+                <Card key={game.id} className="mt-3 w-50 mx-auto shadow">
+                    {game.categories?.map(category =>(
+                        <Badge className="ms-2 mt-1 " color="primary" style={{width: "10%",}}>{category.name}</Badge>
+                    ))}
+                    <Badge className="ms-2 mt-1 " color="info" style={{width: "10%"}}>{game.designer}</Badge>
+                    <CardTitle className="mx-auto fs-2">
+                        <Link to={`/allgames/${game.id}`} style={{textDecoration: 'none', color: "inherit"}}>
+                        {game.title}
+                        </Link>
+                    </CardTitle>
+                </Card>
             )}
         </div>
         </div>
--- src/Managers/gameManager.js
+++ src/Managers/gameManager.js
@@ -1,13 +1,18 @@
-export const FetchAllGames = async () => {
-    const response = await fetch("http://localhost:8000/games",
-        {
-            headers: {
-                Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
-            }
-        })
-    const games = await response.json()
-    return games
-}
+export const FetchAllGames = async (searchQuery = "") => {
+    const url = searchQuery 
+        ? `http://localhost:8000/games?q=${encodeURIComponent(searchQuery)}`
+        : "http://localhost:8000/games";
+    
+    const response = await fetch(url, {
+        headers: {
+            Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
+        }
+    });
+    
+    const games = await response.json();
+    return games;
+};
+
 
 export const FetchSingleGame = async (id) => {
     const response = await fetch(`http://localhost:8000/games/${id}`,{
